2493103e3d3c6bdbc33cb67138b6411a6c4ba347
Bo Bates <bates.bo@gmail.com>
2017-04-16 19:02:20 -0700

    added stuff for gulp and made a simple unit test of 4 examples

---
+++ assets/js/foo-spec.js
@@ -0,0 +1,34 @@
+/* jslint node: true */
+/* global describe, it, expect */
+
+"use strict";
+
+var foo_lib = require('./foo');
+
+describe("#foo", function () {
+  it("Should return true, 'baz' being passed in", function () {
+    var fooed = foo_lib.foo("baz");
+    expect(fooed).toBe(true);
+  });
+})
+
+describe("#foo1", function () {
+  it("Should return false, 'Baz' being passed in", function () {
+    var fooed = foo_lib.foo("Baz");
+    expect(fooed).toBe(false);
+  });
+})
+
+describe("#foo2", function () {
+  it("Should return false, Empty string being passed in", function () {
+    var fooed = foo_lib.foo("");
+    expect(fooed).toBe(false);
+  });
+})
+
+describe("#foo3", function () {
+  it("Should return false, 'Alpha' being passed in", function () {
+    var fooed = foo_lib.foo("Alpha");
+    expect(fooed).toBe(false);
+  });
+})
