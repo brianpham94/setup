3ec3ca42d837fa4ef620a968d03757052da7c653
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-04-17 17:18:27 -0700

    Another test.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 # Only deploy to Firebase if on master branch.
-echo "On $TRAVIS_BRANCH branch."
-if [[ $TRAVIS_BRANCH == $TRAVIS_BRANCH ]]; then
+echo "Deployment script..."
+echo "Checking current branch for deployment."
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
   echo "On $TRAVIS_BRANCH branch... Deploying to Firebase!"
   firebase deploy --project alpha-team-84c76 --token=${FIREBASE_API_TOKEN}
   exit 0;
