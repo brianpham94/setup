d076370d22575a4ebee8a302d7d4f3cc4e96048b
zhi <zhl108@ucsd.edu>
2017-04-22 22:06:54 -0700

    minor update on documentation

--- assets/js/isValidEmail.js
+++ assets/js/isValidEmail.js
@@ -5,12 +5,11 @@
 * @parameter {String} email - the email being validated
 * @return {Boolean} result - true if the email is valid, false if the email is invalid
 *
-* A valid email must be in the form [personalized_part]@[domain] where personalized part is a string composed of any 
-* combination of alphanumeric characters, the symbols “!#$%&'*+-/=?^_`{|}~”. 
+* The regular expression pattern is based on RCF 5322: A valid email must be in the form [personalized_part]@[domain] 
+* where personalized part is a string composed of any combination of alphanumeric characters, the symbols “!#$%&'*+-/=?^_`{|}~”. 
 * The domain is a string composed of any combination of alphanumeric characters, “-”, and “.”.
 * Neither the domain nor the personalized part can have a “.” as the first or last last character or two consecutive “.”. 
-* The whole message body should not exceed the length of 64 characters
-* White space at the beginning and end will be ignored
+* The whole message body should not exceed the length of 64 characters White space at the beginning and end will be ignored
 */
 export.isValidEmail = function(email)
 {
